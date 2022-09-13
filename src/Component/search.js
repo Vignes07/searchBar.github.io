6453a0f5fdd9ded23e5909197bbcae6dc3a36bdd
Vignes Kumar Raj M <vigneskumar@piktorlabs.com>
2022-09-13 15:42:03 +0530

    13/9/2022 3:41PM

--- src/Component/search.js
+++ src/Component/search.js
@@ -1,21 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import "./search.css";
 
 function Search({ data }) {
-  const [result, setResult] = useState([]);
-  var search;
+  const [result, setResult] = useState(data);
+  const [search, setSearch] = useState("");
+  // var search;
+
+  function debounce(func) {
+    let timeout;
+    return function (...args) {
+      const context = this;
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        timeout = null;
+        func.apply(context, args);
+      }, 500);
+    };
+  }
 
   const handleChange = (e) => {
-    search = e.target.value;
+    setSearch(e.target.value);
+    // search = e.target.value;
+    console.log(search);
     const filter = data.filter((value) => {
       return value.name.toLowerCase().includes(search.toLowerCase());
     });
 
     if (search === "") {
-      setResult([]);
+      setResult(data);
     } else {
       setResult(filter);
     }
+  };
+
+  const debounceCall = useCallback(debounce(handleChange), []);
+
+  const bold = (text) => {
+    return text.replace(new RegExp(search, "gi"), (match) =>
+      match.fontcolor("red").bold()
+    );
   };
 
   return (
@@ -24,13 +47,22 @@ function Search({ data }) {
         <input
           type="search"
           placeholder="Enter a Country..."
-          onChange={handleChange}
+          onChange={debounceCall}
         />
       </div>
-      <div id="result">
-        {result.map((value, index) => {
-          return <div key={index}>{value.name}</div>;
-        })}
+      <div>
+        <ul>
+          {result.map((value, index) => {
+            // return <li key={index}>{value.name}</li>;
+            return (
+              <li
+                key={index}
+                id="result"
+                dangerouslySetInnerHTML={{ __html: bold(value.name) }}
+              />
+            );
+          })}
+        </ul>
       </div>
     </>
   );
